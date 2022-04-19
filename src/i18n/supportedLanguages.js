ccb1795dc5054b715bc44a600ebba79db2775ee3
Juan <jpcadena@espol.edu.ec>
2022-04-19 08:40:11 -0500

    test

--- src/i18n/supportedLanguages.js
+++ src/i18n/supportedLanguages.js
@@ -58,49 +58,62 @@ const en = {
   }
   };
 const es = {
-    website_title: "Becas Épico",
-    mainpage_title: "Sistema de Gestión de Becas Épico",
-    nosotros_msg: "Desde ÉPICO seguimos desarrollando más programas de becas e iniciativas para potenciar el talento digital y brindar nuevas y mejores oportunidades a los guayaquileños. Queremos seguir contribuyendo a construir un Guayaquil digital.",
-    contactanos: {
-        title: "Contáctanos",
+    website_title: "Epic Scholarships",
+    mainpage_title: "Epic Scholarship Management System",
+    nos_msg: "At ÉPICO we continue to develop more scholarship programs and initiatives to promote digital talent and provide new and better opportunities for Guayaquil residents. We want to continue contributing to building a digital Guayaquil.",
+    contact_us: {
+        title: "Contact us",
         address_street: "Av. Carlos Julio Arosemena",
         address: "Plaza Guayarte",
         address_city_country: "Guayaquil - Ecuador",
         telephone: "(04) 390 6090",
         email: "info@epico.gob.ec"
     },
-    footer_text: "Todos los derechos reservados ® 2021 ÉPICO",
-    becas:{
-        title: "Aplica a nuestras becas disponibles",
-        application: "¡Aplica aquí!",
-        message: "Puedes aplicar hasta el"
+    footer_text: "All rights reserved ® 2021 EPIC",
+    scholarships:{
+        title: "Apply to our available scholarships",
+        application: "Apply here!",
+        message: "You can apply until"
     },
-    modalRegistro: {
-        title: "¡En hora buena!",
-        body_msg1: "¡Hemos registrado tu aplicación!",
-        body_msg2: "Para seguir con el proceso debes registrarte",
-        footer_msg1: "Lo hare después",
-        footer_msg2: "Registrar"
+    modalRecord: {
+        title: "In good time!",
+        body_msg1: "We have registered your app!",
+        body_msg2: "To continue with the process you must register",
+        footer_msg1: "I'll do it later",
+        footer_msg2: "Register"
     },
     home:{
-        about_us: "Nosotros",
-        becas: "Becas disponibles",
-        login: "Iniciar sesión"
+        about_us: "We",
+        scholarships: "Available scholarships",
+        login: "Login",
+        logout: "Logout"
     },
     login: {
-        title: "Iniciar sesión",
-        login: "Ingresar",
-        register: "Registrarse"
+        title: "Login",
+        button: "Enter",
+        register: "Register",
+        welcome: "Welcome to the Epic Scholarship Management System",
+        forgot_password: "Forgot your password?",
+        no_account: "Don't have an account?",
+        empty_email: "Email cannot be empty",
+        empty_password: "Password cannot be empty"
     },
     register:{
-        title: "Registro",
-        nombre: "Nombres",
-        apellidos: "Apellidos",
-        cedula: "Identificacion",
-        correo: "Correo electrónico",
-        password: "Contraseña",
-        password_confirm: "Confirmar contraseña",
-        button: "Registrar"
+        title: "Create account",
+        name: "Names",
+        surnames: "Surnames",
+        cedula: "Identification",
+        mail: "Email",
+        password: "Password",
+        button: "Sign up",
+        msg: "Already have an account?",
+        login: "Login"
+    },
+    restore:{
+      title: "Reset Password",
+      email: "Email",
+      msg: "You will receive an email with a link to reset your password.",
+      send: "Send Email"
     }
   };
 
